c1cce1cf286e24d40da4b4e480fe575f8c21e7cf
Wilson <makoaewilson@gmail.com>
2020-04-21 22:31:51 +0200

    added the grapdql data successfuly on to the app

--- src/components/Comp.vue
+++ src/components/Comp.vue
@@ -1,10 +1,29 @@
 <template>
 
-  <div>
+ <div>
+    <form action="" @submit.prevent="mounted()">
+      <div class="col-sm-6">
+        <input v-model="city" city="" type="text" class="form-control">
+        <button type="submit" name="button">Submit</button>
+      </div>
+    </form>   
 
-    <p>Hello World</p>
-      {{directory}}
-
+    <table class="table table-striped mt-4">
+      <thead>
+      <tr>
+        <th scope="col">name</th>
+        <th scope="col">title</th>
+        <th scope="col">city</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="input in directory.edges" :key="input.id">
+        <td>{{ input.node.employeeName }}</td>
+        <td>{{ input.node.employeeTitle.titleName }}</td>
+        <td>{{ input.node.employeeCity.cityName }}</td>
+      </tr>
+      </tbody>
+    </table>
   </div>
 
 </template>
@@ -15,10 +34,11 @@ import axios from 'axios';
     name: 'CompanyData',
     data(){
       return {
+          city: '', 
           directory: []
       }
   },
-
+  methods: {
   async mounted () {
       try {
         var result = await axios({
@@ -27,11 +47,17 @@ import axios from 'axios';
                     data: {
                 query: `
     				{
-					  allTitles {
+					  allEmployees(employeeCity_CityName: "`+this.city+`"){
 					    edges {
 					      node {
 					        id
-					        titleName          
+                  employeeName
+                  employeeTitle {
+                    titleName
+                  }
+                  employeeCity  {
+                    cityName
+                  }          
 					      }
 					    }
 					  }
@@ -39,12 +65,13 @@ import axios from 'axios';
     				`
                 }
             });
-            this.directory = result.data.data.allTitles;
+            this.directory = result.data.data.allEmployees;
         } catch (error) {
             console.error(error)
         }
     }
   }
+}
 
       
 
